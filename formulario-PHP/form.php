3173add04bab3bca10eee8110e9cb9277dd08495
AlanAlves04 <111518984+AlanAlves04@users.noreply.github.com>
2023-03-29 11:24:20 -0300

    php

--- formulario-PHP/form.php
+++ formulario-PHP/form.php
@@ -4,12 +4,30 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Formulario PHP</title>
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
-    <?php 
+    <div class="formulario">
+
+        <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
+
+        Nome: <input type="text" name="nome"><br><br>
+
+        Sobrenome: <input type="text" name="sobrenome"><br><br>
+
+        Email: <input type="email" name="email"><br><br>
+
+        Senha: <input type="password" name="senha"><br><br>
+
+        <button type="submit" name="enviar-formulario">Enviar</button>
+
+        <?php
+            if(isset($_POST['enviar-formulario']));
+        ?>
+
+        </form>
+
+    </div>
     
-    ?>
     
-</body>
-</html>
